0d45e48ab58c5f81f7ec0bed5ef6d26dfca1b36b
KenanJones <kjones1303@volmail.jalc.edu>
2017-03-01 13:48:31 -0600

    Add files via upload
    builds the basic file structure

---
+++ edu/jalc/automobile/engine/CrankShaft.java
@@ -0,0 +1,9 @@
+public class Crankshaft{
+	//private final Material material; //needs the material class
+	private final double weight;
+
+	private Crankshaft(){}
+	public Crankshaft(double weight){
+		this.weight = weight;
+	}
+}
