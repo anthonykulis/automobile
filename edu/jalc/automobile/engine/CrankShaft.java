612108b98b8d163de38fa17bc2bde290b1ea1340
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-03-06 07:52:54 -0600

    update from anthony

--- edu/jalc/automobile/engine/CrankShaft.java
+++
@@ -1,9 +0,0 @@
-public class Crankshaft{
-	//private final Material material; //needs the material class
-	private final double weight;
-
-	private Crankshaft(){}
-	public Crankshaft(double weight){
-		this.weight = weight;
-	}
-}
