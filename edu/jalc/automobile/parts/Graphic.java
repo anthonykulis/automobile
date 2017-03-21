375c0e443af76d204edf895f9a9a3cf54e57eba6
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-03-21 08:34:51 -0500

    Graphic

---
+++ edu/jalc/automobile/parts/Graphic.java
@@ -0,0 +1,20 @@
+package edu.jalc.automobile.parts;
+
+public class Graphic{
+  private final String graphic;
+
+  public Graphic(String graphic){
+    this.graphic = graphic;
+  }
+
+  public String getGraphic(){return this.graphic;}
+
+  String setGraphic(String graphic){
+    this.graphic = graphic;
+    return this.graphic;
+  }
+
+  public String toString(){
+    return "Adding graphic";
+  }
+}
