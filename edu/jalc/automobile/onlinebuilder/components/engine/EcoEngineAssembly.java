146767e59ffcea9daed6cb4cbe150ee0c9192c2d
Hodge-zf <xx.skcs@gmail.com>
2017-03-21 08:27:01 -0500

    Directory overhauled

---
+++ edu/jalc/automobile/onlinebuilder/components/engine/EcoEngineAssembly.java
@@ -0,0 +1,8 @@
+package edu.jalc.automobile.onlinebuilder.components.enginecomponents;
+
+import edu.jalc.automobile.parts.exhaust.EconomyExhaust;
+import edu.jalc.automobile.parts.engine.EcoEngine;
+
+abstract public class EcoEngineAssembly extends EngineAssembly{
+  private EconomyExhaust exhaust;
+}
