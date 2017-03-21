44680e9dbbca06c80753cf63cf8518a7a1afa35e
Hodge-zf <xx.skcs@gmail.com>
2017-03-21 15:40:56 -0500

    Standard Eco Engine

--- edu/jalc/automobile/onlinebuilder/components/engine/economy/standard/StandardEcoEngine.java
+++ edu/jalc/automobile/onlinebuilder/components/engine/economy/standard/StandardEcoEngine.java
@@ -1,7 +1,25 @@
 package edu.jalc.automobile.onlinebuilder.components.engine.economy.standard;
 
 import edu.jalc.automobile.parts.induction.NaturallyAspiratedInduction;
+import edu.jalc.automobile.parts.exhaust.EconomyExhaust;
+import edu.jalc.automobile.parts.engine.EcoEngine;
 
-abstract public class StandardEcoEngine extends BoostedEcoEngine{
+abstract public class StandardEcoEngine extends EcoEngineAssembly{
   private NaturallyAspiratedInduction intake;
+
+  public StandardEcoEngine(EcoEngine engine, EconomyExhaust exhaust, NaturallyAspiratedInduction intake)
+  {
+   super(engine,exhaust);
+   this.intake = intake;
+  }
+
+ public NaturallyAspiratedInduction getIntake()
+ {
+   return this.intake;
+ }
+
+  public void getIntake(NaturallyAspiratedInduction intake)
+  {
+  this.intake = intake;
+  }
 }
