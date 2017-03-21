a408eb604e10ba256a0a208c1e49ca972d3a00b9
Hodge-zf <xx.skcs@gmail.com>
2017-03-21 08:48:05 -0500

    creates standard directory

---
+++ edu/jalc/automobile/onlinebuilder/components/engine/economy/standard/StandardEcoEngine.java
@@ -0,0 +1,7 @@
+package edu.jalc.automobile.onlinebuilder.components.engine.economy.standard;
+
+import edu.jalc.automobile.parts.induction.NaturallyAspiratedInduction;
+
+abstract public class StandardEcoEngine{
+  private NaturallyAspiratedInduction intake;
+}
