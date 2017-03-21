66c0ddf62cb37338951f0cff448075f7748b0c1c
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-03-21 08:22:32 -0500

    delete duplicate folder

--- edu/jalc/automobile/onlinebuilder/components/engine/SportEngineAssembly.java
+++ edu/jalc/automobile/onlinebuilder/components/engine/SportEngineAssembly.java
@@ -1,4 +1,4 @@
-package edu.jalc.automobile.onlinebuilder.components.enginecomponents;
+package edu.jalc.automobile.onlinebuilder.components.engine;
 
 import edu.jalc.automobile.parts.engineparts.exhaust.PerformanceExhaust;
 
