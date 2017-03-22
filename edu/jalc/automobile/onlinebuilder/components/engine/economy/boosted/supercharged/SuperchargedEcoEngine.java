dcfdac78731ca5c3391e3a4c5daba47890cc33d6
Hodge-zf <xx.skcs@gmail.com>
2017-03-22 08:20:05 -0500

    Constructor fix

--- edu/jalc/automobile/onlinebuilder/components/engine/economy/boosted/supercharged/SuperchargedEcoEngine.java
+++ edu/jalc/automobile/onlinebuilder/components/engine/economy/boosted/supercharged/SuperchargedEcoEngine.java
@@ -1,9 +1,22 @@
 package edu.jalc.automobile.onlinebuilder.components.engine.economy.boosted.supercharged;
 
+import edu.jalc.automobile.parts.engine.EcoEngine;
 import edu.jalc.automobile.parts.induction.SuperchargedInduction;
+import edu.jalc.automobile.parts.exhaust.EconomyExhaust;
 
 abstract public class SuperchargedEcoEngine extends BoostedEcoEngine{
   private SuperchargedInduction induction;
 
+  public SuperchargedEcoEngine(EcoEngine engine, EconomyExhaust exhaust, SuperchargedInduction induction){
+    super(engine,exhaust);
+    this.induction = induction;
+  }
 
+  public SuperchargedInduction getInduction(){
+    return this.induction;
+  }
+
+  public void setInduction(SuperchargedInduction induction){
+   this.induction = induction;
+  }
 }
--- edu/jalc/automobile/onlinebuilder/components/engine/economy/boosted/turbocharged/TurbochargedEcoEngine.java
+++ edu/jalc/automobile/onlinebuilder/components/engine/economy/boosted/turbocharged/TurbochargedEcoEngine.java
@@ -1,7 +1,22 @@
 package edu.jalc.automobile.onlinebuilder.components.engine.economy.boosted.turbocharged;
 
+import edu.jalc.automobile.parts.engine.EcoEngine;
 import edu.jalc.automobile.parts.induction.TurbochargedInduction;
+import edu.jalc.automobile.parts.exhaust.EconomyExhaust;
 
 abstract public class TurbochargedEcoEngine extends BoostedEcoEngine{
   private TurbochargedInduction induction;
+
+  public TurbochargedEcoEngine(EcoEngine engine, EconomyExhaust exhaust, TurbochargedInduction induction){
+    super(engine,exhaust);
+    this.induction = induction;
+  }
+
+  public TurbochargedInduction getInduction(){
+    return this.induction;
+  }
+
+ public void setInduction(TurbochargedInduction induction){
+   this.induction = induction;
+ } 
 }
