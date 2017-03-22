b0ce386bda2879660c1829be03bf0758b78dd4fc
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-03-21 21:02:06 -0500

    Luxury and Standard Cabin

---
+++ edu/jalc/automobile/parts/LuxuryCabin.java
@@ -0,0 +1,12 @@
+package edu.jalc.automobile.parts;
+
+public class LuxuryCabin extends Cabin{
+
+  public LeatherSeat getSeat(){
+    return super.leatherSeat;
+  }
+  LeatherSeat setSeat(){
+    super.leatherSeat;
+    return super.leatherSeat;
+  }
+}
