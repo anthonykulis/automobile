9c5ee01fd578b095ce9d30a6da0222804ea28463
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-03-21 22:03:01 -0500

    Suv

---
+++ edu/jalc/automobile/onlinebuilder/components/body/Suv.java
@@ -0,0 +1,19 @@
+package edu.jalc.automobile.onlinebuilder.components.body;
+
+public class Suv extends Body{
+
+  public Suv(Quarterpanels quarterpanels, EngineCompartment engineCompartment,
+                                Cabin cabin, StandardTrunk standardTrunk){
+    super(quarterpanels, engineCompartment, cabin, standardTrunk);
+  }
+
+  public StandardTrunk getStandardTrunk(){
+    return (StandardTrunk) this.getTrunk();
+  }
+
+  abstract Cabin getCabin(){}
+
+  public String toString(){
+    return "Adding StandardTrunk to Suv"
+  }
+}
