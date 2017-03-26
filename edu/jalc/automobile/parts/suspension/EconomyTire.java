ea58b1aae83a756e15d6fe4b9a1fa795b5ae14f2
Anthony <anthony.kulis@gmail.com>
2017-03-25 23:50:05 -0500

    fixes #13

--- edu/jalc/automobile/parts/suspension/EconomyTire.java
+++ edu/jalc/automobile/parts/suspension/EconomyTire.java
@@ -2,27 +2,14 @@ package edu.jalc.automobile.parts.suspension;
 
 public class EconomyTire extends Tire {
 
-   private double height;
-   private double width;
-   
    private EconomyTire(){
-      this.height = 0;
-      this.width = 0;
+      super(0, 0);
    }
-   
+
    public EconomyTire(double height, double width){
-      this.height = height;
-      this.width = width;
+      super(height, width);
    }
-   
-   public double getHeight(){
-      return height;
-   }
-   
-   public double getWidth(){
-      return width;
-   }
-   
+
    public String toString(){
       return "Economy Tire";
    }
--- edu/jalc/automobile/parts/suspension/Tire.java
+++ edu/jalc/automobile/parts/suspension/Tire.java
@@ -3,5 +3,19 @@ package edu.jalc.automobile.parts.suspension;
 import edu.jalc.automobile.onlinebuilder.components.suspension.Suspension;
 
 public abstract class Tire extends Suspension {
+	private double height;
+	private double width;
 
+	public Tire(double height, double width){
+		this.height = height;
+		this.width = width;
+	}
+
+	public double getHeight(){
+		return height;
+	}
+
+	public double getWidth(){
+		return width;
+	}
 }
