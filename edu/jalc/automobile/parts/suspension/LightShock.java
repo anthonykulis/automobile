5854f1d83f0d80d715fc11337ec5c3b4f2101ea3
Anthony <anthony.kulis@gmail.com>
2017-03-26 00:34:07 -0500

    fixes induced errors in light shock from refactoring

--- edu/jalc/automobile/parts/suspension/LightShock.java
+++ edu/jalc/automobile/parts/suspension/LightShock.java
@@ -2,6 +2,10 @@ package edu.jalc.automobile.parts.suspension;
 
 public class LightShock extends Shock {
 
+   public LightShock(double height){
+      super(height);
+   }
+
    public String toString(){
       return "Light Shock";
    }
