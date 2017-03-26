478cf144c8b6a310b5dd71143225d5dc4654e8ef
Anthony <anthony.kulis@gmail.com>
2017-03-26 00:32:47 -0500

    fixes induced errors in heavy shock from refactoring

--- edu/jalc/automobile/parts/suspension/HeavyShock.java
+++ edu/jalc/automobile/parts/suspension/HeavyShock.java
@@ -2,6 +2,10 @@ package edu.jalc.automobile.parts.suspension;
 
 public class HeavyShock extends Shock {
 
+   public HeavyShock(double height){
+      super(height);
+   }
+
    public String toString(){
       return "Heavy Shock";
    }
