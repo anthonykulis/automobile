8d6ecdedd046924b5727eeed95060c27d1cb682e
Anthony <anthony.kulis@gmail.com>
2017-03-25 23:53:07 -0500

    fixes #22

--- edu/jalc/automobile/parts/suspension/MediumSpring.java
+++ edu/jalc/automobile/parts/suspension/MediumSpring.java
@@ -3,14 +3,8 @@ package edu.jalc.automobile.parts.suspension;
 public class MediumSpring extends Spring{
 
   public MediumSpring(double height) {
-    this.height = height;
+    super(height);
   }
-
-   private double height;
-
-   public double getHeight(){
-      return height;
-   }
 
    public String toString(){
       return height+ " inch Coil Spring";
