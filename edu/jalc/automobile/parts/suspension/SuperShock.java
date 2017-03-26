851c6f8677c2266fac5834abe39c9fb925d974bf
Anthony <anthony.kulis@gmail.com>
2017-03-26 00:36:44 -0500

    fixes induced errors in supershock from refactoring

--- edu/jalc/automobile/parts/suspension/SuperShock.java
+++ edu/jalc/automobile/parts/suspension/SuperShock.java
@@ -2,6 +2,7 @@ package edu.jalc.automobile.parts.suspension;
 
 public class SuperShock extends Shock{
 
+   public SuperShock(double height){ super(height); }
    public String toString(){
       return "Super Shock";
    }
