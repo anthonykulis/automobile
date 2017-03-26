116c1aec4c3b618201c7ba7c48ebfd995e941c24
Anthony <anthony.kulis@gmail.com>
2017-03-26 00:26:37 -0500

    fixes #28

--- edu/jalc/automobile/parts/suspension/Shock.java
+++ edu/jalc/automobile/parts/suspension/Shock.java
@@ -2,4 +2,11 @@ package edu.jalc.automobile.parts.suspension;
 
 public abstract class Shock{
 
+	private double height;
+
+	public Shock(double height){
+		this.height = height;
+	}
+
+	public double getHeight(){ return height; }
 }
--- edu/jalc/automobile/parts/suspension/ShortShocks.java
+++ edu/jalc/automobile/parts/suspension/ShortShocks.java
@@ -2,13 +2,11 @@ package edu.jalc.automobile.parts.suspension;
 
 public class ShortShocks extends Shock{
 
-   private final double height = 2;
-
-   public double getHeight(){
-      return height;
+   public ShortShocks(double height){
+      super(height);
    }
 
    public String toString(){
-      return height + " inch shocks";
+      return this.getHeight() + " inch shocks";
    }
 }
