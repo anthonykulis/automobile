50140b03c354fd4b910cb8de6757333efc326c7f
Anthony <anthony.kulis@gmail.com>
2017-03-26 00:30:35 -0500

    fixes induced errors in allteraintire from refactoring

--- edu/jalc/automobile/parts/suspension/AllTerrainTire.java
+++ edu/jalc/automobile/parts/suspension/AllTerrainTire.java
@@ -2,26 +2,15 @@ package edu.jalc.automobile.parts.suspension;
 
 public class AllTerrainTire extends Tire {
 
-   private double height;
-   private double width;
    
    private AllTerrainTire(){
-      this.height = 0;
-      this.width = 0;
+      super(0,0);
    }
    
    public AllTerrainTire(double height, double width){
-      this.height = height;
-      this.width = width;
+      super(height, width);
    }
-   
-   public double getHeight(){
-      return height;
-   }
-   
-   public double getWidth(){
-      return width;
-   }
+
    
    public String toString(){
       return "All Terrain Tire";
