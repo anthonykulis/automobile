83722f00c4cfeb2f4df5e02dbc0fe10872ebac58
Anthony <anthony.kulis@gmail.com>
2017-03-25 23:54:11 -0500

    fixes #31

--- edu/jalc/automobile/parts/suspension/SteelWheel.java
+++ edu/jalc/automobile/parts/suspension/SteelWheel.java
@@ -2,27 +2,15 @@ package edu.jalc.automobile.parts.suspension;
 
 public class SteelWheel extends Wheel {
 
-   private double size;
-   private Tire tire;
    
    private SteelWheel(){
-      this.size = 0;
-      this.tire = null;
-      
+      super(0, null);
    }
    
    public SteelWheel(double size, Tire tire){
-      this.size = size;
-      this.tire = tire;
+      super(size, tire);
    }
-   
-   public double getSize(){
-      return size;
-   }
-   
-   public Tire getTire(){
-      return tire;
-   }
+
    
    public String toString(){
       return "\tSteal Wheel with " + tire.toString();
