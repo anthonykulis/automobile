17fbfd4fb1a8797dbbcf40b8f263dda6ee9cd976
Anthony <anthony.kulis@gmail.com>
2017-03-26 00:31:28 -0500

    fixes induced errors in extendedshocks from refactoring

--- edu/jalc/automobile/parts/suspension/ExtendedShocks.java
+++ edu/jalc/automobile/parts/suspension/ExtendedShocks.java
@@ -2,7 +2,11 @@ package edu.jalc.automobile.parts.suspension;
 
 public class ExtendedShocks extends Shock {
 
+   public ExtendedShocks(double height){
+      super(height);
+   }
+
    public String toString(){
-      return "12 inch shocks";
+      return "Extended Shocks";
    }
 }
