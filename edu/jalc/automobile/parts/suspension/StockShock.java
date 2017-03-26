204f2e4f4cf186800e3d182578acf2cb783e0c74
Anthony <anthony.kulis@gmail.com>
2017-03-26 00:36:19 -0500

    fixes induced errors in stockshock from refactoring

--- edu/jalc/automobile/parts/suspension/StockShock.java
+++ edu/jalc/automobile/parts/suspension/StockShock.java
@@ -2,6 +2,7 @@ package edu.jalc.automobile.parts.suspension;
 
 public class StockShock extends Shock{
 
+   public StockShock(double height){ super(height); }
    public String toString(){
       return "Stock Shock";
    }
