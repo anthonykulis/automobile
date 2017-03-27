63a3c59cd65f8a6787abec8dcc6707dd2ebe6e00
NickAnson <nick.anson00@gmail.com>
2017-03-27 00:21:21 -0500

    Fixed issues from 62-68
    And moved a file from body due to it no supposed to be there in in suspension instead

--- edu/jalc/automobile/parts/suspension/SuperShock.java
+++
@@ -1,9 +0,0 @@
-package edu.jalc.automobile.parts.suspension;
-
-public class SuperShock extends Shock{
-
-   public SuperShock(double height){ super(height); }
-   public String toString(){
-      return "Super Shock";
-   }
-}
