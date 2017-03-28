bbd1fcafb17f3c635d8a8ae3e276acf40e8164a4
nourelhoda89 <nfarhat6558@volmail.jalc.edu>
2017-03-28 16:34:01 -0500

    fixes #48 1 point,added toString for car and made the truck toString more descriptives

--- edu/jalc/automobile/onlinebuilder/components/body/car/SedanBody.java
+++ edu/jalc/automobile/onlinebuilder/components/body/car/SedanBody.java
@@ -7,8 +7,16 @@ import edu.jalc.automobile.parts.body.StandardTrunk;
 
 public class SedanBody extends CarBody {
 
-  public SedanBody(Quarterpanels quarterpanels, EngineCompartment engineCompartment, Cabin cabin, StandardTrunk trunk){
-    super(quarterpanels, engineCompartment, cabin, trunk);
-  }
+   public SedanBody(Quarterpanels quarterpanels, EngineCompartment engineCompartment, Cabin cabin, StandardTrunk trunk){
+      super(quarterpanels, engineCompartment, cabin, trunk);
+   }
+   public String toString(){
+      return "SedanBody has :\n" +
+                          "\t"+ getQuarterpanels() +"\n"+
+                          "\t"+ getEngineCompartment()+"\n"+
+                          "\t"+ getCabin()+"\n"+
+                          "\t"+ getTrunk();
+   }
+    
 
 }
