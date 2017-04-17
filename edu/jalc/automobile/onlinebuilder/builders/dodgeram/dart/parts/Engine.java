3e08f11be9645db4445ba9bbdf08361b4e374efe
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-04-17 09:05:19 -0500

    Add Engine Part

---
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/dart/parts/Engine.java
@@ -0,0 +1,5 @@
+package edu.jalc.automobile.onlinebuilder.builders.dodgeram.dart.parts;
+
+public class Engine{
+
+}
---
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/dart/parts/TwoLiterI4DOHCEngine.java
@@ -0,0 +1,8 @@
+package edu.jalc.automobile.onlinebuilder.builders.dodgeram.dart.parts;
+
+public class TwoLiterI4DOHCEngine extends Engine{
+
+   public String toString(){
+      return "2.0-Liter I4 DOHC Engine";
+   }
+}
