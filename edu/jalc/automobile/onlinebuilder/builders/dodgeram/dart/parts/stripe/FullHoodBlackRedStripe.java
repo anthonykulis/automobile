91254e263efa5a4dd4b47dc2e28dde474ec86cdd
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-04-17 09:44:54 -0500

    Added stripe parts to dart

---
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/dart/parts/stripe/FullHoodBlackRedStripe.java
@@ -0,0 +1,8 @@
+package edu.jalc.automobile.onlinebuilder.builders.dodgeram.dart.parts.stripe;
+
+public class FullHoodBlackRedStripe extends Stripe{
+
+   public String toString(){
+      return "Full Hood Black/Red Stripe";
+   }
+}
---
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/dart/parts/stripe/RoofHoodGrayStripe.java
@@ -0,0 +1,8 @@
+package edu.jalc.automobile.onlinebuilder.builders.dodgeram.dart.parts.stripe;
+
+public class RoofHoodGrayStripe extends Stripe{
+
+   public String toString(){
+      return "Roof/Hood Gray Stripe";
+   }
+}
