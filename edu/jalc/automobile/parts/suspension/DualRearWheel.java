fc0a433518cc7c2dbfcdcca2539fe779df567e2e
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-04-17 22:54:56 -0500

    DualRearWheel

---
+++ edu/jalc/automobile/parts/suspension/DualRearWheel.java
@@ -0,0 +1,12 @@
+package edu.jalc.automobile.parts.suspension;
+
+public class DualRearWheel extends Wheel{
+
+  private DualRearWheel(double size){
+    super(0);
+  }
+
+  public DualRearWheel(){
+    super(17);
+  }
+}
