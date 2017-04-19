0e200bcd6bfd9cd6b578392b689f801f65ce4d99
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-04-19 08:51:07 -0500

    Adds SportBuilder bare bones

---
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/ram1500/builders/Ram1500SportBuilder.java
@@ -0,0 +1,35 @@
+package edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.builders;
+
+import edu.jalc.automobile.Automobile;
+import edu.jalc.automobile.onlinebuilder.builders.dodgeram.DodgeRamBuilderInterface;
+import edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.parts.axle.*;
+import edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.parts.cabandbox.*;
+import edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.parts.drive.*;
+import edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.parts.exteriorpaint.*;
+import edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.parts.seating.*;
+import edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.parts.tire.*;
+//import new items form Ceaser
+import edu.jalc.automobile.common.utils.prompter.*;
+import java.util.*;
+
+public class Ram1500SportBuilder implements DodgeRamBuilderInterface{
+
+  public DodgeRamBuilderInterface askForPowerTrain(TerminalPrompterBuilderInterface promptBuilder){
+    return null;
+  }
+  public DodgeRamBuilderInterface askForColorAndInterior(TerminalPrompterBuilderInterface promptBuilder){
+    return null;
+  }
+  public DodgeRamBuilderInterface askForOptions(TerminalPrompterBuilderInterface promptBuilder){
+    return null;
+  }
+  public DodgeRamBuilderInterface askForPackages(TerminalPrompterBuilderInterface promptBuilder){
+    return null;
+  }
+
+  public Automobile build(){
+    return null;
+  }
+
+
+}
