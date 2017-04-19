330a1ff4a6e24b38c4ded10d6cff9762c8729d26
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-04-19 08:07:44 -0500

    Matte Wheel Class Deleted

--- edu/jalc/automobile/onlinebuilder/builders/dodgeram/ram1500/parts/wheels/SeventeenMatteWheel.java
+++
@@ -1,8 +0,0 @@
-package edu.jalc.automobile.onlinebuilder.builders.dodgeram.ram1500.parts.wheels;
-
-public class SeventeenMatteWheel extends Wheels{
-
-  public String toString(){
-    return "17-Inch x 8-Inch Matte Black Aluminum";
-  }
-}
