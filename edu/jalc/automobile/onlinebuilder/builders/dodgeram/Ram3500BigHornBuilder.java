1dd550d75d2c338a85a3146ce3ff6c0d6ee1a4ca
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-04-18 21:30:22 -0500

    Ram3500BigHornBuilder start

---
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/Ram3500BigHornBuilder.java
@@ -0,0 +1,7 @@
+package edu.jalc.automobile.onlinebuilder.builders.dodgeram;
+
+public class Ram3500BigHornBuilder implements DodgeRamBuilderInterface{
+  public DodgeRamBuilderInterface askForPowerTrain(TerminalPrompterBuilderInterface promptBuilder){
+    
+  }
+}
