e054beeb3f36b2b2597634c653e5cf841d0df1ac
Brandon Henk <brandonhenk@Brandons-MacBook-Air.local>
2017-04-23 13:53:56 -0500

    Adding methods to Ram3500LaramieLongHornBuilder

--- edu/jalc/automobile/onlinebuilder/builders/dodgeram/Ram3500LaramieLongHornBuilder.java
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/Ram3500LaramieLongHornBuilder.java
@@ -20,6 +20,7 @@ import edu.jalc.automobile.parts.exhaust.EconomyExhaust;
 import edu.jalc.automobile.parts.induction.TurbochargedInduction;
 import edu.jalc.automobile.parts.body.seat.Seat;
 import edu.jalc.automobile.parts.driveline.*;
+import edu.jalc.automobile.parts.drive.*;
 
 
 
@@ -33,6 +34,51 @@ public class Ram3500LaramieLongHornBuilder implements DodgeRamBuilderInterface{
   private Wheel wheel;
   private Wheel rearWheel;
   private Tire tire;
+  private TruckCabandBed truckCabandBed;
+  private Drive drive;
+  private Body body;
+
+
+  public Ram3500LaramieLongHornBuilder askForTruckDrive(){
+
+    TerminalPrompterBuilder truckDriveBuilder = new TerminalPrompterBuilder();
+
+    try{
+      truckDriveBuilder.newBuilder().addType("TruckDrive")
+        .addOption(new FourByFourDrive())
+        .addOption(new FourByTwoDrive())
+        .sort()
+        .build()
+        .ask();
+    } catch(Exception e){
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    drive = (Drive)truckDriveBuilder.getOptions().get(0);
+    return this;
+  }
+
+  public Ram3500LaramieLongHornBuilder askForCabandBed(){
+
+    TerminalPrompterBuilder cabAndBedBuilder = new TerminalPrompterBuilder();
+
+    try{
+      cabAndBedBuilder.newBuilder().addType("Cab and Bed Length")
+        .addOption(new TruckCabandBed("Crew Cab", 8))
+        .addOption(new TruckCabandBed("Crew Cab", 6.3))
+        .addOption(new TruckCabandBed("Mega Cab", 6.3))
+        .sort()
+        .build()
+        .ask();
+    } catch(Exception e){
+      e.printStackTrace();
+      System.exit(1);
+    }
+
+    truckCabandBed = (TruckCabandBed)cabAndBedBuilder.getOptions().get(0);
+    return this;
+  }
 
   public Ram3500LaramieLongHornBuilder askForPowerTrain(){
 
