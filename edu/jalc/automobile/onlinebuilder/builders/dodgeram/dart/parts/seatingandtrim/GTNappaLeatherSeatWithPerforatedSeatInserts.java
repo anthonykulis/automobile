fa0222751b1093e480bad9b76977068a6a8a9826
E231 <E231@volnet.jalcdom.jal.cc.il.us>
2017-04-25 08:53:22 -0500

    Fixed null

--- edu/jalc/automobile/onlinebuilder/builders/dodgeram/dart/parts/seatingandtrim/GTNappaLeatherSeatWithPerforatedSeatInserts.java
+++ edu/jalc/automobile/onlinebuilder/builders/dodgeram/dart/parts/seatingandtrim/GTNappaLeatherSeatWithPerforatedSeatInserts.java
@@ -1,8 +1,8 @@
 package edu.jalc.automobile.onlinebuilder.builders.dodgeram.dart.parts.seatingandtrim;
 
-import edu.jalc.automobile.parts.body.seat.ClothSeat;
+import edu.jalc.automobile.parts.body.seat.LeatherSeat;
 
-public class GTNappaLeatherSeatWithPerforatedSeatInserts extends ClothSeat{
+public class GTNappaLeatherSeatWithPerforatedSeatInserts extends LeatherSeat{
 
    public String toString(){
       return "GT Nappa Leather Seat with Perforated Seat Inserts";
